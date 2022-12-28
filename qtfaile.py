1baab35536dc2cf5d9ab50678b4747029a3ecbda
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-12-28 22:43:34 +0200

    pyqt

--- qtfaile.py
+++ qtfaile.py
@@ -9,6 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_MainWindow(object):
@@ -222,9 +223,32 @@ class Ui_MainWindow(object):
 
 
     def results(self):
-        res = eval(self.label_result.text())
-        self.label_result.setText("Результат:" + str(res))
-        self.is_equal = True
+        if not self.is_equal:
+            res = eval(self.label_result.text())
+            self.label_result.setText("Результат:" + str(res))
+            self.is_equal = True
+        else:
+            error = QMessageBox()
+            error.setWindowTitle("Ошибка")
+            error.setText("Сейчас это действие выполнить нельзя")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Reset | QMessageBox.Cancel | QMessageBox.Ok)
+
+            error.setDefaultButton(QMessageBox.Ok)
+            error.setInformativeText("Два раза действие не выполнить")
+            error.setDetailedText("Детали")
+
+            error.buttonClicked.connect(self.popup_action)
+
+            error.exec_()
+
+
+    def popup_action(self, btn):
+        if btn.text() == "Ok":
+            print("Print ok")
+        elif btn.text() == "Reset":
+            self.label_result.setText("")
+            self.is_equal = False
 
 
 if __name__ == "__main__":
